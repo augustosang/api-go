0eb9ecfa0cba20bd87f70692224ad7e5eb940bad
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-18 18:32:38 -0300

    feat: adding loggers for requests info

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,26 +1,31 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"net/http"
 
+	"github.com/augustosang/api-go/src/configuration/logger"
 	"github.com/augustosang/api-go/src/configuration/validation"
 	"github.com/augustosang/api-go/src/model/dtos"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest dtos.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"),
+		)
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
 
-	fmt.Println(userRequest)
 	response := dtos.UserResponse{
 		ID:    "teste",
 		Email: userRequest.Email,
@@ -28,5 +33,9 @@ func CreateUser(c *gin.Context) {
 		Age:   userRequest.Age,
 	}
 
-	c.JSON(201, response)
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
+
+	c.JSON(http.StatusOK, response)
 }
