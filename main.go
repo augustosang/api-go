645fff0522d5514c390fca56d4d8358f5d74ecc1
Augusto Santana Guilherme <augusto.santana.guilherme5@gmail.com>
2023-12-18 13:34:02 -0300

    feat: initialize the routes

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 
+	"github.com/augustosang/api-go/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -10,5 +12,13 @@ func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
 	}
 }
